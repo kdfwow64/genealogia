fec745408ded0b4a99ad593596febad76f4aff3e
Curtis Delicata <curtis.delicata@outlook.com>
2020-07-07 20:13:59 +0000

    About routes and page

---
+++ client/src/js/pages/about/Index.vue
@@ -0,0 +1,22 @@
+<template>
+<div>
+<p>
+Genealogia is a secure online website which you can create your own
+ family tree(s) with. It is designed to have extensive
+features but be easy and accessible to use. You can easily import
+ and export GEDCOM files, view and manipulate the data.
+</p>
+<p>
+It has a tree viewer and more features are planned such as DNA support
+ and the inclusion of archive databases and
+collections of records such as census, birth, marriage and death records.
+</p>
+<p>
+It is aimed to be affordable with a 30 day no obligation trial with different
+ pricing levels depending on how many trees
+you require. There is no limit to the size of your trees and it should
+ easily support hundreds of thousands of records
+any issues you can always contact our support team.
+</p>
+</div>
+</template>
---
+++ client/src/js/routes/about.js
@@ -0,0 +1,14 @@
+import routeImporter from '@core/modules/importers/routeImporter';
+
+const routes = routeImporter(require.context('./about', false, /.*\.js$/));
+const RouterView = () => import('@core/bulma/pages/Router.vue');
+
+export default {
+    path: '/about',
+    component: RouterView,
+    meta: {
+        breadcrumb: 'about',
+        route: 'about.index',
+    },
+    children: routes,
+};
