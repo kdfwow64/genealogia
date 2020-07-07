a617d377b4cdb645ed444177583e5066ed2b9b3c
Curtis Delicata <curtis.delicata@outlook.com>
2020-07-07 20:13:59 +0000

    About routes and page

---
+++ client/src/js/routes/about/index.js
@@ -0,0 +1,12 @@
+const AboutIndex = () => import('../../pages/about/Index.vue');
+
+export default {
+    name: 'about.index',
+    path: '',
+    component: AboutIndex,
+    meta: {
+        breadcrumb: 'index',
+        title: 'About Us',
+    },
+};
+
