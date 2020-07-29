e8a0735c49e220d98288f57782b3c910fb2e78a7
Curtis Delicata <curtis.delicata@outlook.com>
2020-07-30 00:07:48 +0100

    Another fix

--- routes/api.php
+++ routes/api.php
@@ -301,11 +301,11 @@ Route::middleware(['api', 'auth', 'core', 'multitenant'])
                 Route::get('', 'Index')->name('index');
                 Route::get('create', 'Create')->name('create');
                 Route::post('', 'Store')->name('store');
-                Route::get('{mediaobject}/edit', 'Edit')->name('edit');
+                Route::get('{mediaobjects}/edit', 'Edit')->name('edit');
 
-                Route::patch('{mediaobject}', 'Update')->name('update');
+                Route::patch('{mediaobjects}', 'Update')->name('update');
 
-                Route::delete('{mediaobject}', 'Destroy')->name('destroy');
+                Route::delete('{mediaobjects}', 'Destroy')->name('destroy');
 
                 Route::get('initTable', 'InitTable')->name('initTable');
                 Route::get('tableData', 'TableData')->name('tableData');
