2ac84ef6d63cb15f2f5bad9b295733ef84621ac8
Curtis Delicata <curtis.delicata@outlook.com>
2020-07-29 20:17:41 +0100

    People table only show tenant data if tenant

--- app/Tables/Builders/PersonTable.php
+++ app/Tables/Builders/PersonTable.php
@@ -3,19 +3,25 @@
 namespace App\Tables\Builders;
 
 use App\Person;
+use App\Traits\ConnectionTrait;
 use Illuminate\Database\Eloquent\Builder;
 use LaravelEnso\Tables\Contracts\Table;
 
 
 class PersonTable extends \LaravelEnso\People\Tables\Builders\PersonTable
 {
+    use ConnectionTrait;
+
     protected const TemplatePath = __DIR__.'/../Templates/people.json';
 
     public function query(): Builder
     {
-        return Person::selectRaw('
+        $conn = $this->getConnection();
+        $db = $this->getDB();
+
+        return Person::on($conn)->selectRaw('
             people.id, people.title, people.givn, people.surn,  people.appellative, people.email, people.phone,
-            people.birthday, CASE WHEN users.id is null THEN 0 ELSE 1 END as "user",
+            people.birthday, people.deathday, CASE WHEN users.id is null THEN 0 ELSE 1 END as "user",
             companies.name as company, people.created_at
         ')->leftJoin('users', 'people.id', '=', 'users.person_id')
             ->leftJoin(
