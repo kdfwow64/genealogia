05f293287453326e04c56e4b22a48a8ded2e9509
Curtis Delicata <curtis.delicata@outlook.com>
2020-09-04 19:20:56 +0100

    Remove join and users and companies from people table

--- app/Tables/Builders/PersonTable.php
+++ app/Tables/Builders/PersonTable.php
@@ -23,14 +23,7 @@ class PersonTable extends \LaravelEnso\People\Tables\Builders\PersonTable
             people.id
             , people.title, people.givn, people.surn,  people.appellative, people.email, people.phone,
             people.birthday, people.deathday, CASE WHEN users.id is null THEN 0 ELSE 1 END as "user",
-            companies.name as company, people.created_at
-        ')->leftJoin('users', 'people.id', '=', 'users.person_id')
-            ->leftJoin(
-                'company_person',
-                fn ($join) => $join
-                    ->on('people.id', '=', 'company_person.person_id')
-                    ->where('company_person.is_main', true)
-            )->leftJoin('companies', 'company_person.company_id', 'companies.id');
+            companies.name as company, people.created_at');
     }
 
     public function templatePath(): string
