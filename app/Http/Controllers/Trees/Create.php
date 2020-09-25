9f98f79f6c3ba6d6a2ddb3b23918044b46aaa2a3
Curtis Delicata <curtis.delicata@outlook.com>
2020-09-25 14:21:32 +0100

    Create security fix

--- app/Http/Controllers/Trees/Create.php
+++ app/Http/Controllers/Trees/Create.php
@@ -4,11 +4,31 @@ namespace App\Http\Controllers\Trees;
 
 use Illuminate\Routing\Controller;
 use App\Forms\Builders\TreeForm;
+use Illuminate\Support\Facades\Auth;
+use App\Tree;
 
 class Create extends Controller
 {
     public function __invoke(TreeForm $form)
     {
-        return ['form' => $form->create()];
+        $user = Auth::user();
+
+        if ($user->role_id == 2 || $user->role_id == 4 || $user->role_id == 5) {
+            if(Tree::where('user_id', '=', $user->user_id)->count() < 1){
+                return ['form' => $form->create()];
+            }
+        }
+
+        if ($user->role_id == 6 || $user->role_id == 7) {
+            if(Tree::where('user_id', '=', $user->user_id)->count() < 10){
+                return ['form' => $form->create()];
+            }
+        }
+
+        if ($user->role_id == 1 || $user->role_id == 8 || $user->role_id == 9) {
+            return ['form' => $form->create()];
+        }
+
+
     }
 }
