a23fc0ab86901a3e7736c2f826ea236d9047d4da
Curtis Delicata <curtis.delicata@outlook.com>
2020-10-01 10:14:40 +0100

    Multi tenancy for built in addresses

--- routes/api.php
+++ routes/api.php
@@ -891,3 +891,25 @@ Route::namespace('Events')
 
     });
 
+Route::middleware(['api', 'auth', 'core', 'multitenant'])
+    ->prefix('api/core/addresses')->as('core.addresses.')
+    ->namespace('\LaravelEnso\Addresses\Http\Controllers')
+    ->group(function () {
+        Route::get('localities', 'Localities')->name('localities');
+        Route::get('regions', 'Regions')->name('regions');
+        Route::get('', 'Index')->name('index');
+        Route::get('create', 'Create')->name('create');
+        Route::post('', 'Store')->name('store');
+        Route::get('options', 'Options')->name('options');
+        Route::get('postcode', 'Postcode')->name('postcode');
+        Route::get('{address}/edit', 'Edit')->name('edit');
+        Route::get('{address}/localize', 'Localize')->name('localize');
+        Route::patch('{address}', 'Update')->name('update');
+        Route::delete('{address}', 'Destroy')->name('destroy');
+
+        Route::patch('makeDefault/{address}', 'MakeDefault')->name('makeDefault');
+        Route::patch('makeBilling/{address}', 'MakeBilling')->name('makeBilling');
+        Route::patch('makeShipping/{address}', 'MakeShipping')->name('makeShipping');
+
+        Route::get('{address}', 'Show')->name('show');
+    });
