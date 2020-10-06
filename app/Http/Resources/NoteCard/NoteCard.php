d74401f8443eda944b8dc3e0c0e62781a9c414d8
Curtis Delicata <curtis.delicata@outlook.com>
2020-10-06 20:20:48 +0100

    Update to NoteCard.php

--- app/Http/Resources/NoteCard/NoteCard.php
+++ app/Http/Resources/NoteCard/NoteCard.php
@@ -10,20 +10,9 @@ class NoteCard extends JsonResource
     {
         return [
             'id' => $this->id,
-            'country' => $this->relationLoaded('country') ? $this->country->name : null,
-            'region' => $this->relationLoaded('region') ? optional($this->region)->name : null,
-            'locality' => $this->relationLoaded('locality') ? optional($this->locality)->name : null,
-            'city' => $this->city,
-            'street' => $this->street,
-            'additional' => $this->resource->additional,
-            'postcode' => $this->postcode,
-            'notes' => $this->notes,
+            'name' => $this->name,
+            'description' => $this->description,
             'isDefault' => $this->is_default,
-            'isShipping' => $this->is_shipping,
-            'isBilling' => $this->is_billing,
-            'label' => $this->relationLoaded('region') && $this->relationLoaded('locality')
-                ? $this->label()
-                : null,
         ];
     }
 }
